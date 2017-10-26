43a479749456488e532b15972a3106f56537f9ef
Yuvaraj <yuvaraj119@gmail.com>
2017-10-26 18:17:26 +0530

    completed home screen container

--- app/components/Container/styles.js
+++ app/components/Container/styles.js
@@ -1,5 +1,12 @@
 import EStyleSheet from "react-native-extended-stylesheet";
 
-const styles = EStyleSheet.create({});
+const styles = EStyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '$primaryBlue'
+    }
+});
 
 export default styles;
