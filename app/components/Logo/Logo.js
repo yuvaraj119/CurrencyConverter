f2971b3d13cf8da74a9f5ce7906853fc11a470f0
Yuvaraj <yuvaraj119@gmail.com>
2017-10-27 20:10:45 +0530

    working on logo

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import styles from "./styles";
+
+
+const Logo = () => (
+    <View style={styles.container}>
+        <Image resizeMode="contain" style={styles.containerImage} source={require('./images/background.png')}>
+            <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')}/>
+        </Image>
+        <Text style={styles.text}>Currency Converter</Text>
+    </View>
+);
+
+export default Logo;
