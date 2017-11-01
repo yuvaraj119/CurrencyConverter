bcf8a953c9d077ca24e0d06e4d2f5717d2287a4e
Yuvaraj Yadav <yuvaraj119@gmail.com>
2017-11-01 09:39:59 +0530

    Redux Navigation completed

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import {Provider} from 'react-redux';
+import { Provider, connect } from 'react-redux';
+import { addNavigationHelpers } from 'react-navigation';
 
 import Navigator from './config/routes';
 import store from './config/store';
@@ -20,8 +21,24 @@ EStyleSheet.build({
   // $outline: 1,
 });
 
+const App = ({ dispatch, nav }) => (
+  <Navigator
+    navigation={addNavigationHelpers({
+      dispatch,
+      state: nav,
+    })}
+  />
+);
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+const AppWithNavigation = connect(mapStateToProps)(App);
+
 export default () => (
   <Provider store={store}>
-    <Navigator onNavigationStateChange={null} />
+    <AppWithNavigation />
+    {/* <Navigator onNavigationStateChange={null} /> */}
   </Provider>
 );
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,11 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 import currenciesR from './currenciesR';
 import themeR from './themeR';
+import nav from './nav';
 
 export default combineReducers({
-    currenciesR, themeR
+  currenciesR,
+  themeR,
+  nav,
 });
