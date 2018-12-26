95c2bfbe5fbcac98216aa165fdb03614f266bfd3
Yuvaraj <yuvaraj.kumar@colanonline.com>
2018-12-26 13:13:11 +0530

    changed admob

--- app/components/admob/Admob.js
+++ app/components/admob/Admob.js
@@ -6,63 +6,63 @@ import styles from './styles';
 
 
 class Admob extends Component {
-    static propTypes = {
-      adType: PropTypes.string,
-    };
+  static propTypes = {
+    adType: PropTypes.string,
+  };
 
-    componentDidMount() {
-      NetInfo.isConnected.fetch()
-        .then((isConnected) => {
-          console.log(`Connection is ${isConnected ? 'Expensive' : 'Not Expensive'}`);
-          AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
-          AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId]);
-          AdMobInterstitial.requestAd().then(() => AdMobInterstitial.showAd().).catch((error) => {
-            console.error(error);
-          });
-        }).catch((error) => {
+  componentDidMount() {
+    NetInfo.isConnected.fetch()
+      .then((isConnected) => {
+        console.log(`Connection is ${isConnected ? 'Expensive' : 'Not Expensive'}`);
+        AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+        AdMobInterstitial.setTestDevices([AdMobInterstitial.simulatorId]);
+        AdMobInterstitial.requestAd().then(() => AdMobInterstitial.showAd().catch((error) => {
           console.error(error);
-        });
-    }
+        }));
+      }).catch((error) => {
+        console.error(error);
+      });
+  }
 
-    componentWillUnmount() {
-      AdMobInterstitial.removeAllListeners();
-    }
+  componentWillUnmount() {
+    AdMobInterstitial.removeAllListeners();
+  }
 
-    showAdBanner = () => {
+  showAdBanner = () => {
 
-      let showBanner=false;
-      NetInfo.isConnected.fetch()
-        .then((isConnected) => {
-          console.log(`Connection is ${isConnected ? 'Expensive' : 'Not Expensive'}`,isConnected);
-          showBanner=true;
-        }).catch((error) => {
-          console.error('error');
-        });
+    let showBanner = false;
+    NetInfo.isConnected.fetch()
+      .then((isConnected) => {
+        console.log(`Connection is ${isConnected ? 'Expensive' : 'Not Expensive'}`, isConnected);
+        showBanner = true;
+      }).catch((error) => {
+        console.error('error');
+      });
 
-          return (
-              <View>
-                  {/*<Text style={styles.title}>Hello World !</Text>*/}
-                  <AdMobBanner
-                    adSize="largeBanner"
-                    adUnitID="ca-app-pub-3940256099942544/6300978111"
-                    testDevices={[AdMobBanner.simulatorId]}
-                    onAdFailedToLoad={this.handleAdFailedToLoad()}
-                   />
-              </View>);
+    return (
+      <View>
+        {/*<Text style={styles.title}>Hello World !</Text>*/}
+        <AdMobBanner
+          adSize="largeBanner"
+          adUnitID="ca-app-pub-3940256099942544/6300978111"
+          testDevices={[AdMobBanner.simulatorId]}
+          onAdFailedToLoad={this.handleAdFailedToLoad()}
+        />
+      </View>);
 
-    };
+  };
 
-    handleAdFailedToLoad = () =>{
-        console.log("Error for banner");
-    };
+  handleAdFailedToLoad = () => {
+    console.log("Error for banner");
+  };
 
-    render() {
-      return (
-        <View style={styles.container}>
-          {this.showAdBanner()}
-        </View>
-      );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.showAdBanner()}
+      </View>
+    );
+  }
 }
 
 export default Admob;
